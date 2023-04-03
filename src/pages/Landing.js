0a8cfe93b3cdbd181ec7fd4194198d9a237cb5a2
setarehbabajani <setarehbabajani81@gmail.com>
2023-04-03 23:22:23 +0430

    changes in landingpage

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,13 +1,24 @@
 import { Box, Button, Container, createTheme, FormControlLabel, Icon, InputAdornment, TextField, ThemeProvider, Typography } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
+import Typical from "react-typical";
 import './Landing.css';
+
 
 
 export default function Landing(){
     
     return ( 
         <div className="landback">
+
+            <nav className="navbar navbar-expand-lg">
+                <form>
+                    <button className="button-order login" role="button" formAction="./login">Login</button> 
+                    <button className="button-order" role="button" formAction="./sign-up">Sign up</button>
+                </form>
+            </nav>
+
+
             <Container className="container">
                 <Box className="landbox">
                     <Typography variant="h2" 
@@ -24,7 +35,13 @@ export default function Landing(){
                         className="landtext"
                         style={{ fontSize: '18px', color: 'white'}}
                         >
-                            Good food shouldn't cost a fortune . Try us and see
+                            <Typical
+                            loop={Infinity}
+                            wrapper="b"
+                            steps={['Delicious food that wont break the bank.', 3000,'Affordable meals that wont sacrifice taste.',3000,'Good food shouldnt cost a fortune.',3000]}
+                            />
+                            <br></br>
+                            Try us and see
                             <span role="img" aria-label="eye"> 👀</span>
                     </Typography>
                     <form>
