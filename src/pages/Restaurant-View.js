7e279bcf933b2358b8310b91467894c643aaf607
helia-vafaei <h.vafaie123@gmail.com>
2023-04-09 16:38:13 +0330

    adding card restaurant

--- src/pages/Restaurant-View.js
+++ src/pages/Restaurant-View.js
@@ -3,7 +3,6 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import Switch from '@mui/material/Switch';
@@ -17,7 +16,17 @@ import { styled, alpha } from '@mui/material/styles';
 import {Badge } from "@material-ui/core";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import './Restaurant-View.css';
-
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Avatar from '@mui/material/Avatar';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 const Search = styled('div')(({ theme }) => ({
     position: 'relative',
@@ -26,11 +35,11 @@ const Search = styled('div')(({ theme }) => ({
     '&:hover': {
         backgroundColor: alpha(theme.palette.common.white, 0.25),
     },
-    marginLeft: 0, // added this line to center the search box
-    marginRight: theme.spacing(8), // added this line to add some margin between the search box and the user icon
+    marginLeft: 0, 
+    marginRight: theme.spacing(8), 
     width: '100%',
     [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1), // adjusted this value to match the default margin of the Toolbar
+        marginLeft: theme.spacing(1), 
         width: 'auto',
     },
 }));
@@ -50,7 +59,6 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     color: 'inherit',
     '& .MuiInputBase-input': {
         padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
         paddingLeft: `calc(1em + ${theme.spacing(4)})`,
         transition: theme.transitions.create('width'),
         width: '100%',
@@ -63,98 +71,134 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 },
 }));
 
+interface ExpandMoreProps extends IconButtonProps {
+    expand: boolean;
+}
+const ExpandMore = styled((props: ExpandMoreProps) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+    }),
+}));
+
+
 const RestaurantView = () => 
 {
     const [auth, setAuth] = React.useState(true);
     const [anchorEl, setAnchorEl] = React.useState('');
-
+    const [expanded, setExpanded] = React.useState(false);
 
     const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
     };
-
     const handleClose = () => {
     setAnchorEl(null);
     };
 
     return (
-    <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" className="headerRV">
-        <Toolbar className='toolbarRV'>
-            
-            {/* <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            >
-            <MenuIcon />
-            </IconButton> */}
-            {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}> */}
-            <img 
-                className='logo'
-                src="/logo4.png"
-                alt="NoWaste"
-            />
-            {/* </Typography> */}
+    <div>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" className="headerRV">
+            <Toolbar className='toolbarRV'>
 
-            <Search>
-                <SearchIconWrapper>
-                <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase
-                placeholder="Search…"
-                inputProps={{ 'aria-label': 'search' }}
+                <img 
+                    className='logo'
+                    src="/logo4.png"
+                    alt="NoWaste"
                 />
-            </Search>
-            {auth && (
-            <div >
-                
-                <IconButton color='inherit'>
-                            <Badge badgeContent={2} color='error'>
-                                <ShoppingCartIcon />
-                            </Badge>
+
+                <Search>
+                    <SearchIconWrapper>
+                    <SearchIcon />
+                    </SearchIconWrapper>
+                    <StyledInputBase
+                    placeholder="Search…"
+                    inputProps={{ 'aria-label': 'search' }}
+                    />
+                </Search>
+                {auth && (
+                <div >
+                    
+                    <IconButton color='inherit'>
+                        <Badge badgeContent={2} color='error'>
+                            <ShoppingCartIcon />
+                        </Badge>
+                    </IconButton>
+                    <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleMenu}
+                    color="inherit"
+                    className='last-iconRV'
+                    >
+                    <AccountCircle />
+                    </IconButton>
+                    
+                    <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                    >
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={handleClose}>Log out</MenuItem>
+                    </Menu>
+
+                </div>
+                )}
+            </Toolbar>
+            </AppBar>
+        </Box>
+
+        <Card sx={{ maxWidth: 345, marginLeft: 150, marginTop: 3, marginRight: 3, border: 1, borderColor: '#911436'}}>
+            <CardHeader
+            avatar={
+                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                R
+                </Avatar>
+            }
+            // action={
+            //     <IconButton aria-label="settings">
+            //     <MoreVertIcon />
+            //     </IconButton>
+            // }
+            title="name_restaurant"
+            subheader="From September 14, 2016"
+            />
+            <CardMedia
+            component="img"
+            src="/logo4.png"
+            alt="Restaurant1"
+            />
+            <CardContent>
+            <Typography variant="body2" color="text.secondary">
+                This impressive paella is a perfect party dish and a fun meal to cook
+                together with your guests. Add 1 cup of frozen peas along with the mussels,
+                if you like.
+            </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                <FavoriteIcon />
                 </IconButton>
-                <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-                className='last-iconRV'
-                >
-                <AccountCircle />
-                </IconButton>
-
-                
-                <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-                >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>Log out</MenuItem>
-                </Menu>
-
-            </div>
-            )}
-
-
-        </Toolbar>
-        </AppBar>
-    </Box>
+            </CardActions>
+        </Card>
+    </div>
     );
 }
 
