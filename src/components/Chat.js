40f78861a394b5106e658c7573514d3af9c0841b
helia-vafaei <h.vafaie123@gmail.com>
2023-05-24 16:02:42 +0330

    design chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,35 +1,95 @@
-import * as React from 'react';
-import * as MU from '@mui/material';
+import React, { useState } from 'react';
 import Fab from '@mui/material/Fab';
 import HeadsetMicIcon from '@mui/icons-material/HeadsetMic';
-import { useState } from 'react';
-import './Chat.css';
+// import ReactScrollToBottom from "react-scroll-to-bottom";
+import './Chat.css'
+import Button from '@mui/material/Button';
+import Fade from '@mui/material/Fade';
+import Paper from '@mui/material/Paper';
+import Popper from '@mui/material/Popper';
+import Typography from '@mui/material/Typography';
+import SendIcon from '@mui/icons-material/Send';
 
+const Chat = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [open, setOpen] = React.useState(false);
+    const [placement, setPlacement] = React.useState();
 
-const  Chat= () => {
     const [isChatOpen, setIsChatOpen] = useState(false);
-
     const handleFabClick = () => {
-        setIsChatOpen(true);
+        setIsChatOpen(prevState => !prevState); 
+    };
+
+    const handleClick = (newPlacement) => (event) => {
+        setAnchorEl(event.currentTarget);
+        setOpen((prev) => placement !== newPlacement || !prev);
+        setPlacement(newPlacement);
     };
 
     return (
-    <div>
-        <Fab
-            style={{ backgroundColor: "#ffa600", position: "fixed", left: "20px", bottom: "20px"}}
-            aria-label="add"
-            onClick={handleFabClick}
-        >
-            <HeadsetMicIcon />
-            
-        </Fab>
-        {/* {isChatOpen && (
-                // Render your chat box component here
-            
-        )} */}
+        <div>
+            <Popper open={open} anchorEl={anchorEl} placement={placement} transition >
+            {({ TransitionProps }) => (
+            <Fade {...TransitionProps} timeout={350}>
+                <Paper className='chatPage'>
+                    <div className="chatContainer">
+                        <Typography>
+                            <div className="chat-header">
+                                <h2 className='chat-title'>Support chat</h2>
+                            </div>
+                            <div className="chatBox">
+                                salam
+                            </div>
+                            <div className="inputBox">
+                                <input onKeyPress={(event) => event.key === 'Enter'} type="text" id="chatInput" />
+                                <Button><SendIcon/></Button>
+                            </div>
+                        </Typography>
+                    </div>
+                </Paper>
+            </Fade>
+            )}
+        </Popper>
+        
 
 
-    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <Fab
+                style={{ backgroundColor: "#ffa600", position: "fixed", left: "20px", bottom: "20px" }}
+                aria-label="add"
+                // onClick={handleFabClick}
+                onClick={handleClick('top')}
+            >
+                <HeadsetMicIcon />
+            </Fab>
+            {/* {isChatOpen && (
+                <div className="chatPage">
+                    <div className="chatContainer">
+                    <div className="header">
+                        <h2>Support</h2>
+                        
+                    </div>
+                    </div>
+    
+                </div>
+            )} */}
+        </div>
+
+        
     );
 }
 
