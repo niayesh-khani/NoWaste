7e35d600539b113224bef36e02952171747e8740
setarehbabajani <setarehbabajani81@gmail.com>
2023-07-05 23:28:40 +0430

    changing for post in map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -4,6 +4,7 @@ import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import 'leaflet-defaulticon-compatibility';
 import 'leaflet/dist/leaflet.css';
 import './Map.css';
+import axios from "axios";
 
 function Map(props) {
   const mapRef = useRef(null);
@@ -13,16 +14,59 @@ function Map(props) {
   setLat(props[0]);
   setLng(props[1]);
   const id = props[2];
+  const type = props[3];
+  const token = localStorage.getItem('token');
 
   //update the new location
-  //get the lat and lng
-
   const handleSaveClick = () => {
     if (markerRef.current) {
       const { lat, lng } = markerRef.current.getLatLng();
       setLat(lat);
       setLng(lng);
       console.log('Marker saved position:', lat, lng);
+      const userData = {
+        "lat": lat,
+        "lon": lng
+      }
+      if(type === "customer")
+      {
+        axios.post(`http://5.34.195.16/user/${id}/lat_long/`, userData,        
+          {headers :
+            {
+              'Content-Type' : 'application/json',
+              "Access-Control-Allow-Origin" : "*",
+              "Access-Control-Allow-Methods" : "GET,POST",
+              'Authorization' : "Token " + token.slice(1,-1)
+            }})
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            if (error.response) {
+                console.log(error.response);
+            } 
+        }); 
+      }
+      else
+      {
+        axios.post(`http://5.34.195.16/restaurant/${id}/lat_long/`, userData, 
+          {headers :
+          {
+            'Content-Type' : 'application/json',
+            "Access-Control-Allow-Origin" : "*",
+            "Access-Control-Allow-Methods" : "GET,POST",
+            'Authorization' : "Token " + token.slice(1,-1)
+          }}
+        )
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            if (error.response) {
+                console.log(error.response);
+            } 
+        }); 
+      }
     }
   };
 
