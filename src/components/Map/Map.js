07fbedc1475e451b459ba4d72463de49731f6b19
setarehbabajani <setarehbabajani81@gmail.com>
2023-07-06 22:44:33 +0430

    what the fuck

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -29,7 +29,7 @@ function Map(props) {
       };
 
       const url = type === "customer"
-        ? `http://5.34.195.16/user/${id}/lat_long`
+        ? `http://5.34.195.16/user/${id}/lat_long/`
         : `http://5.34.195.16/restaurant/${id}/lat_long`;
       if(type === "customer")
       {
