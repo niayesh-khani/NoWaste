0572f2f259a2d5f2a152042d83be2c8e14d00b59
haniyehasadi <haniyeasadi627@gmail.com>
2023-07-08 14:12:44 +0330

    dashboard add titles- resolve fav box problem

--- src/components/Dashboard/DashboardRestaurant.js
+++ src/components/Dashboard/DashboardRestaurant.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {Box, createTheme, Divider, FormControl, Grid, Icon, IconButton, Tooltip, InputAdornment, MenuItem, TextField, ThemeProvider, Typography, withStyles } from "@material-ui/core";
+import {Box, createTheme, Grid, IconButton, Tooltip, ThemeProvider, Typography} from "@material-ui/core";
 import '../../pages/EditProfile.css';
 import HeaderRestaurant from '../HeaderRestaurant';
 import '../../pages/EditRestaurant.css';
@@ -7,10 +7,8 @@ import 'react-phone-input-2/lib/style.css';
 import axios from "axios";
 import { useHistory } from "react-router-dom";
 import Footer from "../Footer";
-import {Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, TableSortLabel } from "@mui/material";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import './DashboardRestaurant.css';
-import PropTypes from 'prop-types';
-import { visuallyHidden } from '@mui/utils';
 import { useMemo } from "react";
 import CheckIcon from '@mui/icons-material/Check';
 import ClearIcon from '@mui/icons-material/Clear';
@@ -28,56 +26,7 @@ const theme = createTheme({
         }
     },
 });
-const headCells = [
-    {
-        id: 'no',
-        numeric: true,
-        disablePadding: true,
-        label: 'No.'
-    },
-    {
-        id: 'name',
-        numeric: false,
-        disablePadding: false,
-        label: 'Restaurant name'
-    },
-    {
-        id: 'customer_name',
-        numeric: false,
-        disablePadding: false,
-        label: 'Customer name'
-    },
-    {
-        id: 'customer_email',
-        numeric: false,
-        disablePadding: false,
-        label: 'Customer Email'
-    },
-    {
-        id: 'order',
-        numeric: false,
-        disablePadding: false,
-        label: 'Order'
-    },
-    {
-        id: 'price',
-        numeric: false,
-        disablePadding: false,
-        label: 'Price'
-    },
-    {
-        id: 'date',
-        numeric: false,
-        disablePadding: false,
-        label: 'Date'
-    },
-    {
-        id: "status",
-        numeric: false,
-        disablePadding: false,
-        label: 'Status'
-    },
-];
+
 function createData(name, customer_name, customer_email, order, price, date, status, restaurant_id, order_id) {
     return {
         name,
@@ -92,92 +41,12 @@ function createData(name, customer_name, customer_email, order, price, date, sta
     };
 }  
 
-function descendingComparator(a, b, orderBy){
-    if (b[orderBy] < a[orderBy]){
-        return -1;
-    }
-    if (b[orderBy] > a[orderBy]){
-        return 1;
-    }
-    return 0;
-};
-
-function getComparator(order, orderBy) {
-    return order === 'desc'
-        ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy);
-};
-
-function stableSort(array, comparator) {
-    const stabilizedThis = array.map((el, index) => [el, index]);
-    stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
-        if(order !== 0){
-            return order;
-        }
-        return a[1] - b[1];
-    });
-    return stabilizedThis.map((el) => el[0]);
-};
-
-function DashboardTableHead(props) {
-    const { order, orderBy, rowCount, onRequestSort } = props;
-    const createSortHandler = (property) => (event) => {
-        onRequestSort(event, property);
-    };
-    
-    return (
-        <TableHead>
-            <TableRow>
-                {headCells.map((headCell) => (
-                    <TableCell
-                        key={headCell.id}
-                        align={headCell.numeric ? 'right' : 'left'}
-                        padding={headCell.disablePadding ? 'none' : 'normal'}
-                        sortDirection={orderBy === headCell.id ? order : false}
-                    >
-                        <TableSortLabel 
-                            active={orderBy === headCell.id}
-                            direction={orderBy === headCell.id ? order : 'asc'}
-                            onClick={createSortHandler(headCell.id)}
-                        >
-                            {headCell.label}
-                            {orderBy === headCell.id ? (
-                                <Box component="span" sx={visuallyHidden}>
-                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                                </Box>
-                            ) : null }
-                        </TableSortLabel>
-                    </TableCell>
-                ))}
-            </TableRow>
-        </TableHead>
-    );
-};
-DashboardTableHead.propTypes = {
-    onRequestSort: PropTypes.func.isRequired,
-    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-    orderBy: PropTypes.string.isRequired,
-    rowCount: PropTypes.number.isRequired
-};
-
 export default function DashboardRestaurant(){
-    const [order, setOrder] = useState('asc');
-    const [orderBy, setOrderBy] = useState('Price');
-    const [rowsPerPage, setRowsPerPage] = useState(5);
-    const [page, setPage] =  useState(0);
-    const history = useHistory();
-    const [color, setColor] = useState(localStorage.getItem('avatarColor') || getRandomColor());
     const id = localStorage.getItem('id');
     const token = localStorage.getItem('token');
     const [orderHistory, setOrderHistory] = useState();
     const [currentPage, setCurrentPage] = useState(1);
     const ordersPerPage = 5;
-    function getRandomColor() {
-        const colors = ['#FFA600', '#fff2bf', '#ffe480', '#a2332a' , '#E74C3C' , '#690000' , '#595959', '#3e3e3e' , '#C6C6C6', '#ABABAB', '#B9B9B9'];
-        return colors[Math.floor(Math.random() * colors.length)];
-    }
-    // console.log("$$$$$$$$$$$$$$$$$",favoriteRestaurant);
     const [rows, setRows] = useState([]);
     const rowsWithIndex = rows.map((row, index) => ({ ...row, index }));
     const indexOfLastOrder = currentPage * ordersPerPage;
@@ -185,17 +54,15 @@ export default function DashboardRestaurant(){
     const ordersToShow = rowsWithIndex.slice(indexOfFirstOrder, indexOfLastOrder);
     const totalPages = Math.ceil(rowsWithIndex.length / ordersPerPage);
 
-    //sorting
     const [sortConfig, setSortConfig] = useState({ field: null, direction: 'asc' });
     const handleSort = (field) => {
         const newSortConfig = { field, direction: 'asc' };
-        // If the same column is clicked again, toggle the sort direction
         if (sortConfig.field === field && sortConfig.direction === 'asc') {
             newSortConfig.direction = 'desc';
         }
         setSortConfig(newSortConfig);
     };
-    // Sort the data based on the sortConfig state
+
     const sortedData = useMemo(() => {
         if (sortConfig.field) {
             const compareFunction = (a, b) => {
@@ -265,37 +132,12 @@ export default function DashboardRestaurant(){
                 order_id
             );
             }
-            return null; // Skip invalid orders
+            return null; 
         });
 
-          setRows(newRows.filter(Boolean)); // Filter out null rows
+          setRows(newRows.filter(Boolean)); 
         }
     }, [orderHistory]);
-
-    const handleRequestSort = (e, property) => {
-        const isAsc = orderBy === property && order === "asc";
-        setOrder(isAsc ? 'desc' : 'asc');
-        setOrderBy(property);
-    };
-
-    const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
-
-    const visibleRows = useMemo(
-        () => 
-            stableSort(rows, getComparator(order, orderBy)).slice(
-                page * rowsPerPage , page * rowsPerPage + rowsPerPage,
-            ),
-        [order, orderBy, page, rowsPerPage]
-    );
-    
-    const handleChangePage = (e, newPage) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (e) => {
-        setRowsPerPage(parseInt(e.target.value, 10));
-        setPage(0);
-    };
 
     const getRowColor = (status) => {
         if(status === "Completed") {
@@ -309,17 +151,6 @@ export default function DashboardRestaurant(){
         } 
     };
 
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-    };
     const handleStatus = (status) => {
         if(status === "InProgress")
             return "In progress";
@@ -385,6 +216,7 @@ export default function DashboardRestaurant(){
         <ThemeProvider theme={theme}>
             <div className="dashboard-back">
                 <HeaderRestaurant />
+                <h1 className='dashboard-title'>Dashboard</h1>
                 <Grid container spacing={2} className="dashboard-grid">
                     <Grid item lg={12} md={12} sm={12} xs={12}>
                         <Box className="dashboard-box" id="order-history-box">
@@ -456,10 +288,8 @@ export default function DashboardRestaurant(){
                                     <TableBody>
                                     {sortedData.map((row) => (
                                         <TableRow key={row.order_id}
-                                            // sx={{ cursor: row.status === 'Completed' ? 'pointer' : 'default',}}
                                             tabIndex={-1}
                                             style={{backgroundColor: getRowColor(row.status)}}
-                                            // onClick={() => handleRowClick(row)}
                                         >
                                             <TableCell component="th" scope="row">
                                             {row.name}
